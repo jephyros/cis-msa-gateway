3fe2a1ed8fd90853e4437fb6b904b776798241d7
CIS-Mac_BW <kr.chis@gmail.com>
2020-08-10 11:21:57 +0900

    CircuitBreaker-resilience4j 적용

--- src/main/java/kr/chis/cismsagateway/CisMsaGatewayApplication.java
+++ src/main/java/kr/chis/cismsagateway/CisMsaGatewayApplication.java
@@ -2,10 +2,10 @@ package kr.chis.cismsagateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+
+@EnableCircuitBreaker
 @SpringBootApplication
 public class CisMsaGatewayApplication {
 
